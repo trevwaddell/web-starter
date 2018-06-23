70c45ca2cf4f51ee649e662e2e6acf762b52c253
Trevor Waddell <trevorwaddell@Trevors-iMac.attlocal.net>
2018-06-23 11:31:04 -0400

    added hmr

--- source/index.js
+++ source/index.js
@@ -1,6 +1,11 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
-import App from './components/App';
+import App from "./components/App";
 
-render(<App />, document.querySelector('.app'));
+render(<App />, document.querySelector(".app"));
+
+// Hot Module Replacement
+if (module.hot) {
+  module.hot.accept();
+}
